279c6d8325b4668aa2a98b6d5a700ddc6f36acae
Ben Walters <benjamin.walters@milwaukeetool.com>
2022-10-07 10:06:51 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace HttpSnorlax;
+
+public class Program
+{
+    private static IConfiguration _configuration;
+    private static readonly HttpClient Client = new HttpClient();
+    
+    static async Task Main(string[] args)
+    {
+        var builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json")
+            .AddJsonFile("appsettings.Development.json");
+
+        _configuration = builder.Build();
+        
+        await Process();
+    }
+
+    private static async Task Process()
+    {
+        var url = _configuration["Url"];
+        
+        // Uncomment if you want to use as base URL
+        //Client.BaseAddress = new Uri(url);
+
+        var result = await Client.GetAsync(url);
+
+        if (result.IsSuccessStatusCode)
+        {
+            Console.WriteLine("Request successful!");
+        }
+        else
+        {
+            Console.WriteLine("Request failed! HTTP {0}", result.StatusCode);
+        }
+    }
+}
